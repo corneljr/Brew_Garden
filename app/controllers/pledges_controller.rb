dae3c881e8844388a1216e36963f3581ede542e4
gnazarkin <gleb.nazarkin@gmail.com>
2014-07-15 16:22:01 -0400

    Updated pledges controller

---
+++ app/controllers/pledges_controller.rb
@@ -0,0 +1,29 @@
+class PledgesController < ApplicationController
+  def create
+  	@pledge = @project.pledges.build(pledge_params)
+  	@pledge.user_id = current_user
+
+  end
+
+  def destroy
+  	@pledge = Pledge.find(params[:id])
+  	@pledge.destroy
+  end
+
+  def update
+  	@pledge = Pledge.find(params[:id])
+
+  	if @pledge.update_attributes(pledge_params)
+  		redirect_to project_path(@project)
+  	else
+  		redirect_to project_path(@project)
+  	end
+
+  end
+
+  private
+  def pledge_params
+  	params.require(:pledge).permit(:amount, :reward_id, :user_id)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'pledges/create'
+
+  get 'pledges/destroy'
+
+  get 'pledges/update'
+
   root to: "welcome#index"
 end
