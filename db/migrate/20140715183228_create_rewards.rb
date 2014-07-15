bb310e5665e06da06e3600cbdfb574b3fef512bc
Josh Cornelius <joshcornelius9@gmail.com>
2014-07-15 14:40:47 -0400

    initial commit

---
+++ db/migrate/20140715183228_create_rewards.rb
@@ -0,0 +1,10 @@
+class CreateRewards < ActiveRecord::Migration
+  def change
+    create_table :rewards do |t|
+      t.integer :project_id
+      t.integer :amount
+
+      t.timestamps
+    end
+  end
+end
