b3e401d1d34566d2d4f7de48251d93e42c40bcef
Chad <chadcodes@users.noreply.github.com>
2014-07-16 16:25:13 -0400

    update from name

--- app/mailers/welcome_mailer.rb
+++ app/mailers/welcome_mailer.rb
@@ -1,5 +1,6 @@
 class WelcomeMailer < ActionMailer::Base
-  default from: "hello@brewgarden.com"
+  default from: "\"Brew Garden\" <hello@brewgarden.com>"
+
 
    def welcome_email(user)
     @user = user
