4ff4d2c9de4179615b30cd8ed483421a37dc07f7
davidhurley <dhurls99@gmail.com>
2014-07-17 16:57:54 -0400

    add name to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140717190208) do
+ActiveRecord::Schema.define(version: 20140717205342) do
 
   create_table "comments", force: true do |t|
     t.text     "body"
@@ -72,6 +72,7 @@ ActiveRecord::Schema.define(version: 20140717190208) do
     t.string   "reset_password_token"
     t.datetime "reset_password_token_expires_at"
     t.datetime "reset_password_email_sent_at"
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
