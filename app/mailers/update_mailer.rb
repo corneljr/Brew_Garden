06947452053c89f331206a94ee08d1fb6ce37ab2
Josh Cornelius <joshcornelius9@gmail.com>
2014-07-17 17:38:42 -0400

    Try mailer fix

--- app/mailers/update_mailer.rb
+++ app/mailers/update_mailer.rb
@@ -1,11 +1,11 @@
 class UpdateMailer < ActionMailer::Base
-   default from: "\"Brew Garden\" <hello@brewgarden.com>"
+   # default from: "\"Brew Garden\" <hello@brewgarden.com>"
+    default from: "Brew Garden <hello@brewgarden.com>"
 
-
-   def update_email(project, update)
-   	@project = project
-   	@update = update
-   	project.pledges.each do |pledge|
-    	mail(to: pledge.user.email, subject: "#{project.title} has posted an update!")
-   end
+   # def update_email(project, update)
+   # 	@project = project
+   # 	@update = update
+   # 	project.pledges.each do |pledge|
+   #  	mail(to: pledge.user.email, subject: "#{project.title} has posted an update!")
+   # end
 end
