fce729c21a40d544b2b1899a683440d8be64e8d1
davidhurley <dhurls99@gmail.com>
2014-07-17 16:41:17 -0400

    fix navigation

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,14 +5,15 @@ class SessionsController < ApplicationController
   def create  
     user = login(params[:email], params[:password], params[:remember_me])  
     if user  
-      redirect_back_or_to root_url, :notice => "Logged in!"  
+      redirect_back_or_to root_url, notice: "Logged in!"  
     else  
-      flash.now.alert = "Email or password was invalid."  
+      flash.now[:alert] = "Email or password was invalid." 
+      render 'new'
     end  
   end 
 
   def destroy
 	  session[:user_id] = nil 
-	  redirect_to root_url, :notice => "Logged out!"  
+	  redirect_to root_url, notice: "Logged out!"  
 	end  
 end
