4920d12ab0dd74f982510c4e55667b9ab292ed91
Chad <chadcodes@users.noreply.github.com>
2014-07-18 11:55:25 -0400

    progress bar draw update

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,6 @@
 class WelcomeController < ApplicationController
   def index
-  	@total_funding = Project.all.sum(:funded_amount)
+  	# @total_funding = Project.all.sum(:funded_amount)
+    @most_funded = Project.all.order('funded_amount DESC').limit(3)
   end
 end
