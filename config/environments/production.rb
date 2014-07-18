cbbb205eb2540c5a4e6de97cb4b465072fc10ff6
Josh Cornelius <joshcornelius9@gmail.com>
2014-07-18 10:01:00 -0400

    added carrierwave-aws

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,4 +80,17 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  CarrierWave.configure do |config|
+  config.storage    = :aws
+  config.aws_bucket = ENV['S3_BUCKET_NAME']
+  config.aws_acl    = :public_read
+  config.asset_host = 'http://aqueous-castle-7146.herokuapp.com/'
+  config.aws_authenticated_url_expiration = 60 * 60 * 24 * 365
+
+  config.aws_credentials = {
+    access_key_id:     ENV['AWS_ACCESS_KEY_ID'],
+    secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
+  }
+  end
 end
