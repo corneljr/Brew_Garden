269a1cb001fa6de9103c5f470f2fb127eef45c49
Josh Cornelius <joshcornelius9@gmail.com>
2014-07-20 13:47:34 -0400

    added edit and show views for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,20 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def edit 
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    if @user.save
+      redirect_to @user
+    else
+      render :edit, notice: 'there was an error'
+    end
+  end
+
   def create
     @user = User.new(user_params)
     if @user.save
