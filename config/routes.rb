8b8865dacfa6e2c7f8839125720c6be193ccd668
Josh Cornelius <joshcornelius9@gmail.com>
2014-07-20 17:47:48 -0400

    fixed conflicting comment routes

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   resources :pledges
   resources :sessions, :only => [:new, :create, :destroy]
 	resources :users do 
-    resources :comments
+    resources :comments, except: [:index]
   end
   root to: "welcome#index"
 
