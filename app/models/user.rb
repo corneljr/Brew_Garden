6e4d6e844bb4022139d6c8bb8e53e27301b20f1a
Josh Cornelius <joshcornelius9@gmail.com>
2014-07-20 14:15:41 -0400

    added basic user info and links to user show page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
 	has_many :projects
 	has_many :pledges
+	has_many :comments, as: :commentable
 
 
   authenticates_with_sorcery!
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140720162210) do
+ActiveRecord::Schema.define(version: 20140720175849) do
 
   create_table "comments", force: true do |t|
     t.text     "body"
@@ -75,6 +75,7 @@ ActiveRecord::Schema.define(version: 20140720162210) do
     t.datetime "reset_password_email_sent_at"
     t.string   "name"
     t.string   "avatar"
+    t.text     "description"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
