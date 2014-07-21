7ec0806baded7cf841bd3f027db73096e1f618fb
davidhurley <dhurls99@gmail.com>
2014-07-21 11:30:41 -0400

    style updates with css/js

--- app/assets/javascripts/project_nav.js
+++ app/assets/javascripts/project_nav.js
@@ -1,12 +1,18 @@
 $(document).ready( function() {
 	$('.project-info-link-item').on('click', function(ev){
 		ev.preventDefault();
+		$('.project-info-link-item').parent().removeClass('hnav-on');
+		 $(this).parent().addClass('hnav-on');
+		
 		var text = ev.currentTarget.textContent
+		var attr = text.split(" ")[1];
+
+
 		if (text === 'home'){
-			text = '';
+			attr = '';
 		}
 
-		$.get(window.location.pathname + '/' + text)
+		$.get(window.location.pathname + '/' + attr)
 			.done(function(data){
 				$('.project-container').html(data)
 				$('.slider').slick();
