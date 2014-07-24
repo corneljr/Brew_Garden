268542e6f9bd2e21e292cdd3cee339f1e6edb59f
Josh Cornelius <joshcornelius9@gmail.com>
2014-07-24 08:53:38 -0400

    preparing for merge

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -64,10 +64,10 @@ Rails.application.configure do
     :authentication       => 'plain'
   }
 
-  client = Twitter::Streaming::Client.new do |config|
-    config.consumer_key        = Figaro.env.twitter_consumer_key
-    config.consumer_secret     = Figaro.env.twitter_consumer_secret
-    config.access_token        = Figaro.env.twitter_access_token
-    config.access_token_secret = Figaro.env.twitter_access_token_secret
-  end
+  # client = Twitter::Streaming::Client.new do |config|
+  #   config.consumer_key        = Figaro.env.twitter_consumer_key
+  #   config.consumer_secret     = Figaro.env.twitter_consumer_secret
+  #   config.access_token        = Figaro.env.twitter_access_token
+  #   config.access_token_secret = Figaro.env.twitter_access_token_secret
+  # end
 end
