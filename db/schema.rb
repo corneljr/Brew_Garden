4f877f93f4131fd670369b8ff54ac393b6209c80
Chad <chadcodes@users.noreply.github.com>
2014-07-25 08:38:40 -0400

    search

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140725004121) do
+ActiveRecord::Schema.define(version: 20140724141903) do
 
   create_table "comments", force: true do |t|
     t.text     "body"
