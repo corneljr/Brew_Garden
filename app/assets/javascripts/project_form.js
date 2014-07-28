51b72241d09a922813f1a9047bb87c8e9c443f50
Josh Cornelius <joshcornelius9@gmail.com>
2014-07-28 17:10:02 -0400

    updated save and post button logic

--- app/assets/javascripts/project_form.js
+++ app/assets/javascripts/project_form.js
@@ -1,5 +1,5 @@
 $(document).ready( function(){
-	$('input').on('input', function(){
+	$('input').on('input click', function(){
 		$('.save').removeAttr('disabled')
 		$('.post').attr('disabled','disabled')
   });	
