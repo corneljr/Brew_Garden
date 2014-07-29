4734898baddf62703a70ba22534097e4b8fc9110
Josh Cornelius <joshcornelius9@gmail.com>
2014-07-29 14:30:47 -0400

    realtime image uploading

--- app/assets/javascripts/project_form.js
+++ app/assets/javascripts/project_form.js
@@ -34,5 +34,24 @@ $(document).ready( function(){
   			alert('what');
   });
 
+  $(".logo-input").change(function(){
+    readURL(this);
+  });
+
 });
  
+
+
+function readURL(input) {
+
+  if (input.files && input.files[0]) {
+    var reader = new FileReader();
+
+    reader.onload = function (e) {
+      $('#blah').attr('src', e.target.result);
+    }
+
+    reader.readAsDataURL(input.files[0]);
+  }
+}
+
