708a160bdb2f61f651dfae4b424986c39b3c0ee5
Josh Cornelius <joshcornelius9@gmail.com>
2014-07-30 13:22:33 -0400

    removed pry

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -102,7 +102,6 @@ class ProjectsController < ApplicationController
 		@commentable = find_commentable
   	@comments = @project.comments
   	@location = Geocoder.search(@project.location).first.city
-  	binding.pry
   	@end_date = date_format(Date.today + @project.days_left.days)
 
   	if request.xhr?
