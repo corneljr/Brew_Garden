8f31dcaf8222f10ff42121dcab2df4706e3f2c1a
Josh Cornelius <joshcornelius9@gmail.com>
2014-07-30 11:40:07 -0400

    updated seed file for heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,19 +12,19 @@ addresses = ['10 Sunray Street L1N 9B5 whitby', '130 Riviera Drive L3R 5M1 markh
 # real_entries = ['','','','']
 
 # Hogtown specific
-Project.create(
-  user_id: rand(1..100),
-  title: "Hogtown's Summer of Craft program",
-  short_blurb: 'The demand of Torontonians for craft quality beer and the admiration we have for so many great local micro breweries that are shaking up the beer establishment.',
-  description: 'As for our name, at the turn of the 20th century, Toronto earned the moniker Hogtown for its role in the processing of livestock, particularly the processing and packaging of pork. Toronto has come a long way since then and is now one of the most vibrant global cities, a cultural melting pot with unbelievable tolerance of race, creed and sexual orientation. What Toronto should not continue to tolerate is limited, indistinguishable beer choices from massive foreign-owned breweries.',
-  goal: rand(100000..5000000),
-  days_left: rand(20..60),
-  category: 1,
-  location: 1,
-  post_status: true,
-  logo: '/img/hogtown-logo.png',
-  video_link: 'https://www.youtube.com/watch?v=0lxglqlyxck'
-  )
+# Project.create(
+#   user_id: rand(1..100),
+#   title: "Hogtown's Summer of Craft program",
+#   short_blurb: 'The demand of Torontonians for craft quality beer and the admiration we have for so many great local micro breweries that are shaking up the beer establishment.',
+#   description: 'As for our name, at the turn of the 20th century, Toronto earned the moniker Hogtown for its role in the processing of livestock, particularly the processing and packaging of pork. Toronto has come a long way since then and is now one of the most vibrant global cities, a cultural melting pot with unbelievable tolerance of race, creed and sexual orientation. What Toronto should not continue to tolerate is limited, indistinguishable beer choices from massive foreign-owned breweries.',
+#   goal: rand(100000..5000000),
+#   days_left: rand(20..60),
+#   category: 1,
+#   location: 1,
+#   post_status: true,
+#   logo: '/img/hogtown-logo.png',
+#   video_link: 'https://www.youtube.com/watch?v=0lxglqlyxck'
+#   )
 
 # 100.times do
 # 	User.create(name: Faker::Name.name, email: Faker::Internet.email, password: 'password', password_confirmation: 'password', description: Faker::Lorem.sentences(3).join(' '))
@@ -51,11 +51,11 @@ Project.create(
 # 	project.save
 # end
 
-Project.all.each do |project|
-  project.update_funded_amount
-  project.save
-end
+# Project.all.each do |project|
+#   project.update_funded_amount
+#   project.save
+# end
 
-(1..20).each do |n|
-	SliderImage.create(project_id: n)
-end
+# (1..20).each do |n|
+# 	SliderImage.create(project_id: n)
+# end
