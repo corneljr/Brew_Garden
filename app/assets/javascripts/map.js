92c9fa7a59e0ccb04805e89ebc82f3323dd60b53
Josh Cornelius <joshcornelius9@gmail.com>
2014-07-31 21:11:32 -0400

    subtle changes

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,16 +1,16 @@
-$(document).on('ready page:load', function() {
+// $(document).on('ready page:load', function() {
 
-    var map = L.mapbox.map('map2', 'dhurley99.ipn4bc66').setView([43.652, -79.382], 13);
+//     var map = L.mapbox.map('map2', 'dhurley99.ipn4bc66').setView([43.652, -79.382], 13);
 
-    $.ajax( {
-      url: '/projects',
-      dataType: 'json',
-      type: 'GET',
+//     $.ajax( {
+//       url: '/projects',
+//       dataType: 'json',
+//       type: 'GET',
 
-    }).done(function(data) {
-        map.featureLayer.setGeoJSON(data);
-    });
+//     }).done(function(data) {
+//         map.featureLayer.setGeoJSON(data);
+//     });
 
-   map.scrollWheelZoom.disable();
+//    map.scrollWheelZoom.disable();
 		
-});
+// });
