b6f8d79305168fcd5352bf890d3034f1c490234e
Josh Cornelius <joshcornelius9@gmail.com>
2014-08-01 08:58:12 -0400

    updated user show page for unique projects

--- app/models/pledge.rb
+++ app/models/pledge.rb
@@ -3,4 +3,12 @@ class Pledge < ActiveRecord::Base
 	belongs_to :project
 	belongs_to :reward
 	belongs_to :twitter_reward
+
+	def unique_projects
+		@projects = []
+		self.each do |pledge|
+			@projects.push(pledge.project)
+		end
+		@projects.uniq!
+	end
 end
